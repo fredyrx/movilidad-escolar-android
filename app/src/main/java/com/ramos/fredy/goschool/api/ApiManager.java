d18773069534b64fac89c00386c1d4d764d45e6c
Fredy Ramos <fredy.rx@gmail.com>
2017-04-30 14:45:56 -0500

    Se agregó okhttp3 para el logging interceptor de retrofit. Se agregó Glide para el cargado de imágenes. Se agregó la clase ApiManager para gestionar los endpoints del servicio.

---
+++ app/src/main/java/com/ramos/fredy/goschool/api/ApiManager.java
@@ -0,0 +1,62 @@
+package com.ramos.fredy.goschool.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+/**
+ * Created by kenok on 30/04/2017.
+ */
+
+public class ApiManager {
+
+    private static final String API_URL = "";
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_URL);
+
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofit() {
+        return  retrofit;
+    }
+
+    public static <S> S createService(Class<S> serviceClass) {
+        setupConverter();
+        setupClient();
+        retrofit = builder.build();
+        return retrofit.create(serviceClass);
+    }
+
+    private static void setupConverter() {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+                .create();
+        builder.addConverterFactory(GsonConverterFactory.create(gson));
+    }
+    private static void setupClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.readTimeout(30, TimeUnit.SECONDS);
+        httpClient.connectTimeout(30, TimeUnit.SECONDS);
+        httpClient.addInterceptor(logging);
+
+    }
+
+    public interface ApiClient{
+
+    }
+
+
+}
