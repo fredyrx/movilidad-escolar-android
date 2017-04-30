e18517af9f0c6daead865e393ce16ce8c3f9981e
Fredy Ramos <fredy.rx@gmail.com>
2017-04-29 22:30:38 -0500

    add splash timer

---
+++ app/src/main/java/com/ramos/fredy/goschool/activities/SplashScreenActivity.java
@@ -0,0 +1,44 @@
+package com.ramos.fredy.goschool.activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.ramos.fredy.goschool.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        };
+
+        // Simulate a long loading process
+        Timer timer = new Timer();
+        timer.schedule(task,SPLASH_SCREEN_DELAY);
+
+
+    }
+}
