f9ae7d414e555e0357771ee1ca541c35aa25ab6d
Fredy Ramos <fredy.rx@gmail.com>
2017-04-30 10:46:17 -0500

    basic client and driver registration layout

--- app/src/main/java/com/ramos/fredy/goschool/activities/DriverSignUpActivity.java
+++ app/src/main/java/com/ramos/fredy/goschool/activities/DriverSignUpActivity.java
@@ -1,15 +1,19 @@
 package com.ramos.fredy.goschool.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.ramos.fredy.goschool.R;
 
 public class DriverSignUpActivity extends AppCompatActivity {
+
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,16 @@ public class DriverSignUpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        registerButton = (Button) findViewById(R.id.driver_register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(DriverSignUpActivity.this, LoginActivity.class));
+                finish();
             }
         });
+
     }
 
 }
--- app/src/main/java/com/ramos/fredy/goschool/activities/LoginActivity.java
+++ app/src/main/java/com/ramos/fredy/goschool/activities/LoginActivity.java
@@ -1,15 +1,26 @@
 package com.ramos.fredy.goschool.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.ramos.fredy.goschool.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoginActivity extends AppCompatActivity {
+
+    //@BindView(R.id.client_sign_up_button)
+    Button clientSignUpButton;
+
+    //@BindView(R.id.driver_sign_up_button)
+    Button driverSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,26 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //ButterKnife.bind(this);
+        clientSignUpButton = (Button) findViewById(R.id.client_sign_up_button);
+        driverSignUpButton = (Button) findViewById(R.id.driver_sign_up_button);
+
+        clientSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ClientSignUpActivity.class));
+                finish();
+            }
+        });
+
+        driverSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, DriverSignUpActivity.class));
+                finish();
+            }
+        });
 
     }
 
