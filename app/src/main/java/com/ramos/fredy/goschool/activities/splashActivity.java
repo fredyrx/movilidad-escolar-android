c9931ae8c3925958a81827726f09728867982906
Fredy Ramos <fredy.rx@gmail.com>
2017-04-29 19:19:26 -0500

    add splash

---
+++ app/src/main/java/com/ramos/fredy/goschool/activities/splashActivity.java
@@ -0,0 +1,15 @@
+package com.ramos.fredy.goschool.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ramos.fredy.goschool.R;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
