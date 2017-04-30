a38aa367a6e4f2540dac6f1b5b56a6642321cfc1
Fredy Ramos <fredy.rx@gmail.com>
2017-04-30 18:32:59 -0500

    add global user variable on App class

--- app/src/main/java/com/ramos/fredy/goschool/App.java
+++ app/src/main/java/com/ramos/fredy/goschool/App.java
@@ -3,6 +3,8 @@ package com.ramos.fredy.goschool;
 import android.app.Application;
 
 import com.ramos.fredy.goschool.api.ApiManager;
+import com.ramos.fredy.goschool.models.Client;
+import com.ramos.fredy.goschool.models.Driver;
 
 /**
  * Created by fredy on 30/04/17.
@@ -11,6 +13,33 @@ import com.ramos.fredy.goschool.api.ApiManager;
 public class App extends Application {
 
     private static App instance;
+
+    private Client clientUser;
+    private Driver  driverUser;
+
+    public Client getClientUser() {
+        return clientUser;
+    }
+
+    public void setClientUser(Client clientUser) {
+        this.clientUser = clientUser;
+    }
+
+    public Driver getDriverUser() {
+        return driverUser;
+    }
+
+    public void setDriverUser(Driver driverUser) {
+        this.driverUser = driverUser;
+    }
+
+    public Object getUser(){
+        if (clientUser != null){
+            return clientUser;
+        }else{
+            return driverUser;
+        }
+    }
 
     public App(){
         instance = this;
