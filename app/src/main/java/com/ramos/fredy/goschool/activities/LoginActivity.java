ee75904620760c0f28febbb1f83bd420e0ece7fd
Fredy Ramos <fredy.rx@gmail.com>
2017-05-01 04:34:15 -0500

    Se agregó BaseNavigationDrawerActivity para implementar un drawer. Se pusieron en archivos independientes el toolbar, nav_header_client, activity_client_drawer, activity_driver_drawer. Se agregó la navegación del login al requestservice

--- app/src/main/java/com/ramos/fredy/goschool/activities/LoginActivity.java
+++ app/src/main/java/com/ramos/fredy/goschool/activities/LoginActivity.java
@@ -2,26 +2,15 @@ package com.ramos.fredy.goschool.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.ramos.fredy.goschool.App;
 import com.ramos.fredy.goschool.R;
-import com.ramos.fredy.goschool.api.ApiManager;
-import com.ramos.fredy.goschool.models.io.LoginBody;
-import com.ramos.fredy.goschool.models.io.LoginResponse;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -56,7 +45,10 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_login_sign_in)
     public void sign_in() {
-        ApiManager.ApiClient client = ApiManager.createService(ApiManager.ApiClient.class);
+
+        startActivity(new Intent(this, RequestServiceActivity.class));
+
+        /*ApiManager.ApiClient client = ApiManager.createService(ApiManager.ApiClient.class);
         Call<LoginResponse> call = client.login(new LoginBody());
 
         call.enqueue(new Callback<LoginResponse>() {
@@ -83,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onFailure(Call<LoginResponse> call, Throwable t) {
                 Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
     }
 
 }
